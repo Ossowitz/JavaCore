9bb8d179ee8114397b5a2fafd3e4a6c4e63da0ea
Ossowitz <76myxomor76@gmail.com>
2023-04-05 21:28:34 +0300

    Commit init

---
+++ src/functionalProgramming/functionalInterfaces/PredicateExample.java
@@ -0,0 +1,28 @@
+package functionalProgramming.functionalInterfaces;
+
+import java.awt.*;
+import java.util.function.Predicate;
+
+public class PredicateExample {
+
+    public static void main(String[] args) {
+        Predicate<Integer> lesserThan = new Predicate<Integer>() {
+            @Override
+            public boolean test(Integer i) {
+                return i < 18;
+            }
+        };
+
+        Predicate<Integer> betterThan = new Predicate<Integer>() {
+            @Override
+            public boolean test(Integer i) {
+                return i > 18;
+            }
+        };
+
+        Predicate<Integer> underThan = i -> i < 18;
+
+        Predicate<Integer> largerThan = i -> i > 18;
+
+    }
+}
