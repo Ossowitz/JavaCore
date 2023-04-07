bb697cc16c59b3723ffbb2bcb2f29335b4ae8f11
Ossowitz <76myxomor76@gmail.com>
2023-04-07 21:43:49 +0300

    Commit init

--- src/javaCollectionsFramework/list/ListRunner.java
+++ src/javaCollectionsFramework/list/ListRunner.java
@@ -11,11 +11,18 @@ public class ListRunner {
         List<Integer> linkedList = new LinkedList<>();
         List<Integer> arrayList = new ArrayList<>();
 
-        measure(linkedList);
-        measure(arrayList);
+        measureAdd(linkedList);
+        measureAdd(arrayList);
+
+        measureGet(linkedList);
+        measureGet(arrayList);
+
+        measureAddFirst(linkedList);
+        measureAddFirst(arrayList);
+
     }
 
-    public static void measure(List<Integer> list) {
+    public static void measureAdd(List<Integer> list) {
 
         long start = System.currentTimeMillis();
         for (int i = 0; i < 1_000_000; i++)
@@ -25,4 +32,31 @@ public class ListRunner {
         System.out.println("Insert: " + (end - start));
 
     }
+
+    public static void measureAddFirst(List<Integer> list) {
+
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 1_000_000; i++)
+            list.add(0, i);
+        long end = System.currentTimeMillis();
+
+        System.out.println("Insert: " + (end - start));
+
+    }
+
+    public static void measureGet(List<Integer> list) {
+
+        for (int i = 0; i < 100_000; i++)
+            list.add(i);
+
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 100_000; i++) {
+            list.get(i);
+        }
+        long end = System.currentTimeMillis();
+
+        System.out.println("Get: " + (end - start));
+
+    }
+
 }
