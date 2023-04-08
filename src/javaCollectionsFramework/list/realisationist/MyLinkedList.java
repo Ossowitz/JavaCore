fad6a235bb43097b8ea1ad3a78539b0809e374e8
Ossowitz <76myxomor76@gmail.com>
2023-04-08 22:54:07 +0300

    Commit init

--- src/javaCollectionsFramework/list/realisationist/MyLinkedList.java
+++ src/javaCollectionsFramework/list/realisationist/MyLinkedList.java
@@ -2,8 +2,43 @@ package javaCollectionsFramework.list.realisationist;
 
 public class MyLinkedList {
 
+    private Node head;
+    private int size;
+
+    public void add(int value) {
+        // If this is the first addition to the list
+        if (head == null) {
+            this.head = new Node(value);
+        } else {
+
+        }
+
+        size++;
+    }
+
     private static class Node {
         private int value;
         private Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public void setValue(int value) {
+            this.value = value;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
     }
+
 }
