66d3b5a18825c0f3f8e505163cd5f10b8c6eea68
Ossowitz <76myxomor76@gmail.com>
2023-04-08 22:43:38 +0300

    Commit init

---
+++ src/javaCollectionsFramework/list/realisationist/MyLinkedList.java
@@ -0,0 +1,9 @@
+package javaCollectionsFramework.list.realisationist;
+
+public class MyLinkedList {
+
+    private static class Node {
+        private int value;
+        private Node next;
+    }
+}
