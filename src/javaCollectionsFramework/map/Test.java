8e4ffd2d53fe68563a7e35e8b02a5565342eedfc
Ossowitz <76myxomor76@gmail.com>
2023-04-12 11:34:43 +0300

    Commit init

--- src/javaCollectionsFramework/map/Test.java
+++ src/javaCollectionsFramework/map/Test.java
@@ -6,14 +6,16 @@ import java.util.Map;
 public class Test {
 
     public static void main(String[] args) {
-        Map<Integer, String> map = new HashMap<>();
 
-        map.put(1, "String1");
-        map.put(2, "String2");
-        map.put(3, "String3");
+        Map<Integer, Integer> map = new HashMap<>();
 
-        for (Map.Entry<Integer, String> entry : map.entrySet()) {
-            System.out.println(entry);
-        }
+        map.put(1, 10);
+        map.put(2, 5);
+        map.put(3, 5);
+
+        map.merge(1, 5, (x1, x2) -> (x1 % 2 == 0) ? 1 : 0);
+
+        System.out.println(map);
+
     }
 }
