867d6f549def8f74d9d284f34600533894007217
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-06-07 20:05:22 +0300

    Initial Commit

--- src/logging/UserServlet.java
+++ src/logging/UserServlet.java
@@ -7,7 +7,5 @@ public class UserServlet {
     private static final Logger logger2 = Logger.getLogger(UserServlet.class.getName());
 
     public static void main(String[] args) {
-        logger1.info("message_logger1");
-        logger2.info("message_logger2");
     }
 }
